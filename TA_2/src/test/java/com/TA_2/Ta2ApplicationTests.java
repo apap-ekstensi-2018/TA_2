71507fb2edbf9bccec35ad00f93751893927873d
Yohannes Sakti Panggabean <yoanhans@gmail.com>
2018-04-17 21:02:08 +0700

    Initial Project

---
+++ TA_2/src/test/java/com/TA_2/Ta2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.TA_2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ta2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
