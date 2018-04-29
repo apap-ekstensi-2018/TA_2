d2527a316909c50283690b94d04804d9c229a626
Laborawaty Rajagukguk <laborawaty@bizzy.co.id>
2018-04-29 10:23:19 +0700

    lengkapi fungsi tambah ruang

--- TA_2/src/main/java/com/TA_2/controller/RuangController.java
+++ TA_2/src/main/java/com/TA_2/controller/RuangController.java
@@ -1,17 +1,40 @@
 package com.TA_2.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.TA_2.model.RuangModel;
+import com.TA_2.service.RuangService;
+
 
 @Controller
 public class RuangController {
+	@Autowired
+    RuangService ruangDAO;
+	
+	@RequestMapping("/")
+    public String index ()
+    {
+        return "index";
+    }
+	
 	@RequestMapping("/ruang/tambah")
-    public String add (Model model)
+    public String add ()
     {	
-		model.addAttribute ("mahasiswa", new RuangModel());
         return "tambah-ruang";
     }
+	
+	@RequestMapping("/ruang/tambah/submit")
+    public String addSubmit (
+    			@RequestParam(value = "nama", required = false) String nama,
+            @RequestParam(value = "kapasitas", required = false) int kapasitas)
+    {
+		RuangModel ruang = new RuangModel(nama, kapasitas);
+		
+		ruangDAO.addRuang (ruang);
+        return "tambah-success";
+    }
+	
 }
--- TA_2/src/main/java/com/TA_2/service/RuangServiceDatabase.java
+++ TA_2/src/main/java/com/TA_2/service/RuangServiceDatabase.java
@@ -1,5 +1,22 @@
 package com.TA_2.service;
 
-public class RuangServiceDatabase {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import com.TA_2.dao.RuangMapper;
+import com.TA_2.model.RuangModel;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class RuangServiceDatabase implements RuangService{
+	@Autowired
+    private RuangMapper ruangMapper;
+	
+	@Override
+	public void addRuang(RuangModel ruang) {
+		log.info ("insert ruang");
+		ruangMapper.addRuang(ruang);
+	}
 }
