98370db6f6586c7009527a3baa01481c659afc7d
ar royya <royya22@gmail.com>
2018-05-08 12:59:47 +0700

    fitur 8, 9, 10
    [8] Menampilkan Pengajuan Peminjaman, [9] Menampilkan Riwayat Seluruh Peminjaman Ruang, [10] Menampilkan Riwayat Peminjaman Saya

---
+++ TA_2/src/main/java/com/TA_2/service/PeminjamanRuangServiceDatabase.java
@@ -0,0 +1,55 @@
+package com.TA_2.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.TA_2.dao.PeminjamanRuangMapper;
+import com.TA_2.model.PeminjamanRuangModel;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class PeminjamanRuangServiceDatabase implements PeminjamanRuangService{
+	@Autowired
+    private PeminjamanRuangMapper peminjaman_ruanganMapper;
+	
+	
+	@Override
+	public PeminjamanRuangModel selectPeminjamanRuang(Integer id) {
+		log.info ("select ruang dengan id {}", id);
+        return peminjaman_ruanganMapper.selectPeminjamanRuang(id);
+	}
+
+		
+	@Override
+	public void updatePeminjamanRuang(String is_disetujui, int id) {
+		log.info ("update peminjamanruangan");
+		peminjaman_ruanganMapper.updatePeminjamanRuang(is_disetujui,id);
+	}
+
+
+	@Override
+	public PeminjamanRuangModel selectPeminjamanRuang2(Integer id) {
+		log.info ("select peminjaman ruang dengan id {}", id);
+        return peminjaman_ruanganMapper.selectPeminjamanRuang2(id);
+	}
+
+
+	@Override
+	public List<PeminjamanRuangModel> selectPeminjamanRuangAll() {
+		log.info ("select peminjaman ruang semua");
+        return peminjaman_ruanganMapper.selectPeminjamanRuangAll();
+	}
+
+
+	@Override
+	public List<PeminjamanRuangModel> selectPeminjamanRuangMhs(Integer id) {
+		log.info ("select peminjaman ruang dengan id mhs {}", id);
+        return peminjaman_ruanganMapper.selectPeminjamanRuangMhs(id);
+	}
+
+
+}
